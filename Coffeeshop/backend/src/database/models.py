10039856b193d027378351ef00ea89dd7f0eb374
Benson <benzps01@gmail.com>
2023-07-10 23:49:52 +0530

    comments on models.py

--- Coffeeshop/backend/src/database/models.py
+++ Coffeeshop/backend/src/database/models.py
@@ -1,35 +1,36 @@
 import os
 from sqlalchemy import Column, String, Integer
+from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_filename = "database.db"
+#-----------------------------------------------------------------------------------------------!
+# Database
+#-----------------------------------------------------------------------------------------------!
+load_dotenv()
+database_filename = os.getenv('database_name')
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename)) #type: ignore
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
-
-
+#-----------------------------------------------------------------------------------------------!
+# setup_db(app)
+#     binds a flask application and a SQLAlchemy service
+#-----------------------------------------------------------------------------------------------!
 def setup_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.app = app
+    db.app = app #type: ignore
     db.init_app(app)
 
 
-'''
-db_drop_and_create_all()
-    drops the database tables and starts fresh
-    can be used to initialize a clean database
-    !!NOTE you can change the database_filename variable to have multiple verisons of a database
-'''
-
-
+#-----------------------------------------------------------------------------------------------!
+# db_drop_and_create_all()
+#     drops the database tables and starts fresh
+#     can be used to initialize a clean database
+#     !!NOTE you can change the database_filename variable to have multiple verisons of a database
+#-----------------------------------------------------------------------------------------------!
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
@@ -38,17 +39,13 @@ def db_drop_and_create_all():
         title='water',
         recipe='[{"name": "water", "color": "blue", "parts": 1}]'
     )
-
-
     drink.insert()
-# ROUTES
+    
 
-'''
-Drink
-a persistent drink entity, extends the base SQLAlchemy Model
-'''
-
-
+#-----------------------------------------------------------------------------------------------!
+# Drink Table
+# a persistent drink entity, extends the base SQLAlchemy Model
+#-----------------------------------------------------------------------------------------------!
 class Drink(db.Model):
     # Autoincrementing, unique primary key
     id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
@@ -58,69 +55,66 @@ class Drink(db.Model):
     # the required datatype is [{'color': string, 'name':string, 'parts':number}]
     recipe = Column(String(180), nullable=False)
 
-    '''
-    short()
-        short form representation of the Drink model
-    '''
-
+#-----------------------------------------------------------------------------------------------!
+    # short()
+    #     short form representation of the Drink model
+#-----------------------------------------------------------------------------------------------!
     def short(self):
-        print(json.loads(self.recipe))
-        short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)]
+        #print(json.loads(self.recipe))
+        short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)] #type:ignore
         return {
             'id': self.id,
             'title': self.title,
             'recipe': short_recipe
         }
 
-    '''
-    long()
-        long form representation of the Drink model
-    '''
-
+#-----------------------------------------------------------------------------------------------!
+    # long()
+    #     long form representation of the Drink model
+#-----------------------------------------------------------------------------------------------!
     def long(self):
         return {
             'id': self.id,
             'title': self.title,
-            'recipe': json.loads(self.recipe)
+            'recipe': json.loads(self.recipe) #type:ignore
         }
 
-    '''
-    insert()
-        inserts a new model into a database
-        the model must have a unique name
-        the model must have a unique id or null id
-        EXAMPLE
-            drink = Drink(title=req_title, recipe=req_recipe)
-            drink.insert()
-    '''
-
+#-----------------------------------------------------------------------------------------------!
+    # insert()
+    #     inserts a new model into a database
+    #     the model must have a unique name
+    #     the model must have a unique id or null id
+    #     EXAMPLE
+    #         drink = Drink(title=req_title, recipe=req_recipe)
+    #         drink.insert()
+#-----------------------------------------------------------------------------------------------!
     def insert(self):
         db.session.add(self)
         db.session.commit()
 
-    '''
-    delete()
-        deletes a new model into a database
-        the model must exist in the database
-        EXAMPLE
-            drink = Drink(title=req_title, recipe=req_recipe)
-            drink.delete()
-    '''
 
+#-----------------------------------------------------------------------------------------------!
+    # delete()
+    #     deletes a new model into a database
+    #     the model must exist in the database
+    #     EXAMPLE
+    #         drink = Drink(title=req_title, recipe=req_recipe)
+    #         drink.delete()
+#-----------------------------------------------------------------------------------------------!
     def delete(self):
         db.session.delete(self)
         db.session.commit()
 
-    '''
-    update()
-        updates a new model into a database
-        the model must exist in the database
-        EXAMPLE
-            drink = Drink.query.filter(Drink.id == id).one_or_none()
-            drink.title = 'Black Coffee'
-            drink.update()
-    '''
 
+#-----------------------------------------------------------------------------------------------!
+    # update()
+    #     updates a new model into a database
+    #     the model must exist in the database
+    #     EXAMPLE
+    #         drink = Drink.query.filter(Drink.id == id).one_or_none()
+    #         drink.title = 'Black Coffee'
+    #         drink.update()
+#-----------------------------------------------------------------------------------------------!
     def update(self):
         db.session.commit()
 
