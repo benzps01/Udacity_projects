d936b7fb7ced8ec06cce763f131cacc806d71724
Benson P Sabu <benzps01@gmail.com>
2023-09-01 00:03:31 +0530

    Created all api endpoints for both Movies and Actor models

--- CastingAgency/backend/app.py
+++ CastingAgency/backend/app.py
@@ -33,9 +33,153 @@ def after_request(response):
     return response
 
 
-@app.route("/")
-def show_movies():
+@app.route("/", methods=["GET"])
+def test_point():
     return "This is a test point"
+
+
+def get_movies():
+    movie_list = []
+    all_movies = Movie.query.all()
+    for movie in all_movies:
+        movie_list.append(
+            {
+                "id": movie.id,
+                "title": movie.title,
+                "release_date": movie.release_date,
+                "genre": movie.genre,
+            }
+        )
+    return movie_list
+
+
+def get_actors():
+    actor_list = []
+    all_actors = Actor.query.all()
+    for actor in all_actors:
+        actor_list.append(
+            {
+                "id": actor.id,
+                "name": actor.name,
+                "age": actor.age,
+                "gender": actor.gender,
+            }
+        )
+    return actor_list
+
+
+@app.route("/movies", methods=["GET", "POST"])
+def add_movies():
+    try:
+        if request.method == "POST":
+            title = request.get_json().get("title")
+            release_date = request.get_json().get("release_date")
+            genre = request.get_json().get("genre")
+            new_movie = Movie(title=title, release_date=release_date, genre=genre)
+            new_movie.insert()
+            return jsonify({"success": True, "movie_dict": get_movies()})
+        elif request.method == "GET":
+            return get_movies()
+    except:
+        abort(400)
+
+
+@app.route("/movies/<int:movie_id>", methods=["DELETE"])
+def delete_movie(movie_id):
+    try:
+        movie_to_delete = Movie.query.filter(Movie.id == movie_id).one_or_none()
+
+        if movie_to_delete is None:
+            abort(404)
+        temp = movie_to_delete.title
+        movie_to_delete.delete()
+        return jsonify(
+            {
+                "success": True,
+                "movie_dict": get_movies(),
+                "deleted_movie_title": temp,
+            }
+        )
+    except:
+        abort(400)
+
+
+@app.route("/movies/<int:movie_id>", methods=["PATCH"])
+def update_movie(movie_id):
+    try:
+        movie_to_update = Movie.query.filter(Movie.id == movie_id).one_or_none()
+        if movie_to_update is None:
+            abort(404)
+        title = request.get_json().get("title")
+        release_date = request.get_json().get("release_date")
+        genre = request.get_json().get("genre")
+
+        if title is not None:
+            movie_to_update.title = title
+        if release_date is not None:
+            movie_to_update.release_date = release_date
+        if genre is not None:
+            movie_to_update.genre = genre
+        movie_to_update.update()
+        return jsonify({"success": True, "movie_dict": get_movies()})
+    except:
+        abort(400)
+
+
+@app.route("/actors", methods=["GET", "POST"])
+def add_actors():
+    try:
+        if request.method == "POST":
+            name = request.get_json().get("name")
+            age = request.get_json().get("age")
+            gender = request.get_json().get("gender")
+            new_actor = Actor(name=name, age=age, gender=gender)
+            new_actor.insert()
+            return jsonify({"success": True, "actors_dict": get_actors()})
+        elif request.method == "GET":
+            return get_actors()
+    except:
+        abort(400)
+
+
+@app.route("/actors/<int:actor_id>", methods=["DELETE"])
+def delete_an_actor(actor_id):
+    try:
+        actor_to_delete = Actor.query.filter(Actor.id == actor_id).one_or_none()
+        if actor_to_delete is None:
+            abort(404)
+        temp = actor_to_delete.name
+        actor_to_delete.delete()
+        return jsonify(
+            {"success": True, "actor_dict": get_actors(), "deleted_actor_name": temp}
+        )
+    except:
+        abort(400)
+
+
+@app.route("/actors/<int:actor_id>", methods=["PATCH"])
+def update_actor_details(actor_id):
+    try:
+        actor_to_be_updated = Actor.query.filter(Actor.id == actor_id).one_or_none()
+
+        if actor_to_be_updated is None:
+            abort(404)
+
+        name = request.get_json().get("name")
+        age = request.get_json().get("age")
+        gender = request.get_json().get("gender")
+
+        if name is not None:
+            actor_to_be_updated.name = name
+        if age is not None:
+            actor_to_be_updated.age = age
+        if gender is not None:
+            actor_to_be_updated.gender = gender
+
+        actor_to_be_updated.update()
+        return jsonify({"success": True, "actor_dict": get_actors()})
+    except:
+        abort(400)
 
 
 if __name__ == "__main__":
