38b2e35cc8065c4d5e020e9b7318d9ffe7fca3fa
Benson P Sabu <benzps01@gmail.com>
2023-09-05 23:51:44 +0530

    Added ActorCard, MovieCard and Toggle Button

--- CastingAgency/frontend/src/App.js
+++ CastingAgency/frontend/src/App.js
@@ -1,9 +1,45 @@
-import Card from "./Card";
+import ActorCard from "./ActorCard";
+import MovieCard from "./MovieCard";
+import React, { useEffect, useState } from "react";
+import fetchActorDetails from "./ActorData";
+import fetchMovieDetails from "./MovieData";
+import ToggleButton from "./ToggleButton";
 
 function App() {
+  const [actorDetails, setActorDetails] = useState([]);
+  const [movieDetails, setMovieDetails] = useState([]);
+  const [isOn, setIsOn] = useState(false);
+
+  useEffect(() => {
+    fetchActorDetails()
+      .then((data) => {
+        setActorDetails(data);
+      })
+      .catch((error) => {
+        console.log("Error fetching data:", error);
+      });
+  }, []);
+  useEffect(() => {
+    fetchMovieDetails()
+      .then((data) => {
+        setMovieDetails(data);
+      })
+      .catch((error) => {
+        console.log("Error fetching data:", error);
+      });
+  }, []);
+
+  const updateIsOn = (newIsOn) => {
+    setIsOn(newIsOn);
+  };
   return (
     <div>
-      <Card />
+      <ToggleButton isOn={isOn} updateIsOn={updateIsOn} />
+      {isOn ? (
+        <MovieCard movieDetails={movieDetails} />
+      ) : (
+        <ActorCard actorDetails={actorDetails} />
+      )}
     </div>
   );
 }
