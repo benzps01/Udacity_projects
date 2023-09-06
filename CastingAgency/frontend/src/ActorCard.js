f51ce67fb1a2882b6f59ee87e6d458457ad4c37f
Benson P Sabu <benzps01@gmail.com>
2023-09-06 23:55:12 +0530

    added handle delete function

--- CastingAgency/frontend/src/ActorCard.js
+++ CastingAgency/frontend/src/ActorCard.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Card.css";
+import UpdateActorModal from "./UpdateActorModal";
 
 function ActorCard(props) {
-  const handleDelete() {
+  const [selectedActor, setSelectedActor] = useState(null);
 
-  }
+  const handleUpdateClick = (actor) => {
+    setSelectedActor(actor);
+  };
+
+  const handleDelete = (actor_id) => {
+    axios
+      .delete(`http://127.0.0.1:5000/actors/${actor_id}`)
+      .then((response) => {
+        console.log("Delete Successful", response.data);
+
+        const updateActorList = props.actorDetails.filter(
+          (actor) => actor.id !== actor_id
+        );
+        props.updateActorList(updateActorList);
+      })
+      .catch((error) => {
+        console.log("Delete Actor", error);
+      });
+  };
 
   return (
     <>
@@ -16,12 +36,34 @@ function ActorCard(props) {
             <p>Age: {actor.age}</p>
             <p>Gender: {actor.gender}</p>
           </div>
-          <div className="DU">
-            <button className="update-details" onClick="handleDelete">Update Actor</button>
-            <button className="delete-details">Delete Actor</button>
+          <div className="AU">
+            <button className="add-details" onClick={props.openAddActorModal}>
+              Add Actor
+            </button>
+            <button
+              className="update-details"
+              onClick={() => handleUpdateClick(actor)}
+            >
+              Update Actor
+            </button>
+          </div>
+          <div className="Delete">
+            <button
+              className="delete-details"
+              onClick={() => handleDelete(actor.id)}
+            >
+              Delete Actor
+            </button>
           </div>
         </div>
       ))}
+      {selectedActor && (
+        <UpdateActorModal
+          actor={selectedActor}
+          onClose={() => setSelectedActor(null)}
+          updateActorList={props.updateActorList}
+        />
+      )}
     </>
   );
 }
--- CastingAgency/frontend/src/ActorData.js
+++ CastingAgency/frontend/src/ActorData.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://127.0.0.1:5000/";
+const baseUrl = "http://127.0.0.1:5000";
 
 const fetchActorDetails = async () => {
   try {
