9945c092368ab67106d9a7e6c41cb17c460f3482
Benson P Sabu <benzps01@gmail.com>
2023-09-07 23:56:50 +0530

    implemented auth.py

--- CastingAgency/backend/auth.py
+++ CastingAgency/backend/auth.py
@@ -28,6 +28,20 @@ class AuthError(Exception):
 # -----------------------------------------------------------------------------------------------!
 # Auth Header
 # -----------------------------------------------------------------------------------------------!
+# -----------------------------------------------------------------------------------------------!
+# @TODO implement get_token_auth_header() method
+#     it should attempt to get the header from the request
+#         it should raise an AuthError if no header is present
+#     it should attempt to split bearer and the token
+#         it should raise an AuthError if the header is malformed
+#     return the token part of the header
+
+
+# completed
+# Get Auth tokens, check whether token is "Bearer (token)",
+# raise error if token is not of this format
+# return token value without Bearer keyword
+# -----------------------------------------------------------------------------------------------!
 def get_token_auth_header():
     auth = request.headers.get("Authorization", None)
     if not auth:
@@ -65,7 +79,19 @@ def get_token_auth_header():
 
 
 # -----------------------------------------------------------------------------------------------!
-# check_permissions(permission, payload) method
+# @TODO implement check_permissions(permission, payload) method
+#     @INPUTS
+#         permission: string permission (i.e. 'post:drink')
+#         payload: decoded jwt payload
+
+#     it should raise an AuthError if permissions are not included in the payload
+#         !!NOTE check your RBAC settings in Auth0
+#     it should raise an AuthError if the requested permission string is not in the payload permissions array
+#     return true otherwise
+
+
+# completed
+# Check permissions for the users
 # -----------------------------------------------------------------------------------------------!
 def check_permissions(permission, payload):
     if "permissions" not in payload:
@@ -81,7 +107,22 @@ def check_permissions(permission, payload):
 
 
 # -----------------------------------------------------------------------------------------------!
-# verify_decode_jwt(token) method
+# @TODO implement verify_decode_jwt(token) method
+#     @INPUTS
+#         token: a json web token (string)
+
+#     it should be an Auth0 token with key id (kid)
+#     it should verify the token using Auth0 /.well-known/jwks.json
+#     it should decode the payload from the token
+#     it should validate the claims
+#     return the decoded payload
+
+#     !!NOTE urlopen has a common certificate error described here:
+# https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
+
+
+# completed
+# verify the jwt if it is valid
 # -----------------------------------------------------------------------------------------------!
 def verify_decode_jwt(token):
     jsonurl = urlopen(f"https://{AUTH0_DOMAIN}/.well-known/jwks.json")
@@ -144,7 +185,18 @@ def verify_decode_jwt(token):
 
 
 # -----------------------------------------------------------------------------------------------!
-# @requires_auth(permission) decorator method
+# @TODO implement @requires_auth(permission) decorator method
+#     @INPUTS
+#         permission: string permission (i.e. 'post:drink')
+
+#     it should use the get_token_auth_header method to get the token
+#     it should use the verify_decode_jwt method to decode the jwt
+#     it should use the check_permissions method validate claims and check the requested permission
+#     return the decorator which passes the decoded payload to the decorated method
+
+
+# completed
+# auth permission wrapper
 # -----------------------------------------------------------------------------------------------!
 def requires_auth(permission=""):
     def requires_auth_decorator(f):
@@ -153,6 +205,7 @@ def requires_auth(permission=""):
             token = get_token_auth_header()
             try:
                 payload = verify_decode_jwt(token)
+                print(payload)
             except:
                 abort(401)
             check_permissions(permission, payload)
